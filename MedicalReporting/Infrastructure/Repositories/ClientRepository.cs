fd1b391af9a589e8e9f1c7fa4ca4b5931909215b
Felix Aweh <felix.aweh@hotmail.com>
2025-08-02 15:13:31 +0200

    added infrastructure repositories

---
+++ MedicalReporting/Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,39 @@
+namespace MedicalAssessment.Infrastructure.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        private readonly MedicalAssessmentDbContext _context;
+
+        public ClientRepository(MedicalAssessmentDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Client?> GetByIdAsync(Guid id)
+        {
+            return await _context.Clients
+                .Include(c => c.Assessments)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<List<Client>> GetAllAsync()
+        {
+            return await _context.Clients
+                .Include(c => c.Assessments)
+                .ToListAsync();
+        }
+
+        public async Task<Client> AddAsync(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+            return client;
+        }
+
+        public async Task UpdateAsync(Client client)
+        {
+            _context.Clients.Update(client);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
